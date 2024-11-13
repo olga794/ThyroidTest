be6d5c8bf1777988c1aff78491023b3bf5ccb090
olga794 <125156680+olgaF794@users.noreply.github.com>
2024-11-13 00:04:34 -0500

    commit Inicial

---
+++ app/src/main/java/com/thyroidtest/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.thyroidtest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.thyroidtest.ui.theme.ThyroidTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //EnableEdgeToEdge() extiende la pantalla sobre la barra de opciones
+        setContent {
+            ThyroidTestTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ThyroidTestTheme {
+        Greeting("Android")
+    }
+}
